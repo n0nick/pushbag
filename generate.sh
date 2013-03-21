3d5788829cf4f63aba76690d011285af46fb280a
Sagie Maoz <sagie@maoz.info>
2013-03-21 12:46:54 +0200

    Add generate.sh script to update gh-pages from master

---
+++ generate.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+TFILE=`mktemp /tmp/pushbag.XXXX`
+cp ./README.md $TFILE
+
+git checkout gh-pages
+$POSTFILE=`ls ./_posts/ | head -1`
+
+cp $TFILE ./_posts/$POSTFILE
+git add .
+git commit --all -m "Merge page content from master"
+git push
+
+git checkout master
+echo "Done."
